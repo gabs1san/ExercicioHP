bf7ca4190b7c90a199cf3019abe31aaa2e4a0993
gabs1san <sanbiel398@gmail.com>
2025-05-07 19:25:03 -0300

    Iniciando a interação com o usuario/nao lendo os custos adicionais

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExercicioHP.Entities;
+using System.Globalization;
 
 namespace ExercicioHP
 {
@@ -6,6 +9,45 @@ namespace ExercicioHP
     {
         static void Main(string[] args)
         {
+            List<Employee> list = new List<Employee>();
+
+
+            Console.Write("Enter the number of employess: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Emplouyee #{i} data: ");
+                Console.Write("Outsourced (Y/N)?");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double .Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(ch == 'Y')
+                {
+                    Console.WriteLine("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + "- $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
 
         }
 
